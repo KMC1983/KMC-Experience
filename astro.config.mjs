6443c7cc7079f434789e88589fc4b5f6ab3fffc0
KMC1983 <kevinmcclaxton@gmail.com>
2025-05-12 03:46:20 -0400

    Create  astro.config.mjs
     "Update Astro configuration for GitHub Pages"

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  site: 'https://KMC1983.github.io',
+  base: '/KMC-Experience',
+  integrations: [mdx()],
+});
+
+
+
+
+
+
+
+
+
+ "Update Astro configuration for GitHub Pages"
